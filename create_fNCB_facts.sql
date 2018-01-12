3bb69052062c57505df65b1c2b1ba9018be49ab3
daniel <Daniel.Adler@stim.se>
2018-01-12 15:11:19 +0100

    Bytt namn på filen

---
+++ create_fNCB_facts.sql
@@ -0,0 +1,49 @@
+USE [datalake]
+GO
+/****** Object:  StoredProcedure [dbo].[create_fNCB_facts]    Script Date: 2017-12-12 16:53:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+ALTER PROC [dbo].[create_fNCB_facts] 
+AS
+
+-- Laddning av fNCB_all för alla årtal - Fakta för NCB-avräkning
+-- Anropa med parameter för vilket år som ska laddas
+--
+-- Kräver att stored procedures "create_fAPS_tmptables" och "create_fAPS_temptables_indexes" 
+-- har körts för att hjälptabellerna "for_dm_*" ska ha genererats
+--
+-- Typiskt för att ladda ett år körs:
+-- * create_dimtables
+-- * create_fAPS_tmptables
+-- * create_fAPS_temptables_indexes
+-- * create_fAPS_facts
+-- * create_fRES_facts
+-- * create_fNCB_facts
+
+
+SET NOCOUNT ON
+DECLARE @sql nvarchar(max)
+
+IF OBJECT_ID('datamarts.fNCB_all', 'U') IS NOT NULL
+DROP table datamarts.fNCB_all
+
+SELECT
+	CONVERT(int, 1000000 + mnatnr) AS DistKey,
+	mnkate AS Category,
+	CONVERT(int, mnipnr) AS IceIPNameKey,
+	CONVERT(int, ipi.IPBaseKey) AS IceIPBaseKey,
+	CONVERT(int, mnvknr) AS IceWorkKey,
+	mnbelp AS NcbAmountDistributed
+INTO datamarts.fNCB_all
+FROM dinminpf dm
+LEFT JOIN datamarts.dIPInfoName ipi ON dm.mnipnr = ipi.IPNameKey
+WHERE dm.mnatnr IN (SELECT anatnr FROM dinatnpf WHERE anjbss = '+' AND anjbst IN ('11', '08'))
+
+
+
